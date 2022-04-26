8e532e21f8b3a11e9aa3465217e8974bae71dcd7
Co-Ji <100311029+Co-Ji@users.noreply.github.com>
2022-04-26 15:10:33 +0900

    login, signup 뷰(자리만잡음)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import { history } from '../redux/configureStore';
 
-const Login = () => {
-  return <React.Fragment>Login</React.Fragment>;
+const Login = (props) => {
+  const {} = props;
+
+  return (
+    <Container>
+      <div>
+        <input placeholder="이메일(아이디" />
+      </div>
+      <div>
+        <input placeholder="비밀번호" />
+      </div>
+
+      <div>
+        <button>로그인</button>
+      </div>
+      <div>
+        <button onClick={() => history.push('/signup')}>회원가입</button>
+      </div>
+      <button>카카오로그인</button>
+      <button>구글로그인</button>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 50vw;
+  margin: auto;
+`;
 
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Signup = () => {
-  return <React.Fragment>Signup</React.Fragment>;
+const Signup = (props) => {
+  return (
+    <Container>
+      <div>
+        <input placeholder="아이디" />
+      </div>
+      <div>
+        <input placeholder="닉네임" />
+      </div>
+
+      <div>
+        <input placeholder="비밀번호" />
+      </div>
+      <div>
+        <input placeholder="비밀번호확인" />
+      </div>
+      <span>쌤(ssaem)</span>
+      <input type="checkbox" name="쌤" value="쌤" />
+      <span>생(saeng)</span>
+      <input type="checkbox" name="학생" value="학생" />
+      <div>
+        <button>회원가입</button>
+      </div>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 50vw;
+  margin: auto;
+`;
 
 export default Signup;
