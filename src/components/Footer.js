2e5c7f280f7be494a777ca06ef62d0f3a29d7cbb
crown0205 <crown0205@gmail.com>
2022-04-26 15:00:41 +0900

    header | footer 작업 main 임시로 height 값 줌.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Footer = () => {
-  return <React.Fragment>Footer</React.Fragment>;
+  return (
+    <Wrap>
+      <div className="innerWrap">
+        <h1>Footer</h1>
+      </div>
+    </Wrap>
+  );
 };
 
 export default Footer;
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100px;
+  background: #aaa;
+
+  .innerWrap {
+    width: 90%;
+    max-width: 1400px;
+    height: 100%;
+    margin: auto;
+    padding: 30px 16px 0;
+
+    background: #eee;
+
+    h1 {
+      text-align: center;
+      font-size: 30px;
+    }
+  }
+`;
