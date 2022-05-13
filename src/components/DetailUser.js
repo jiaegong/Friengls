eb6f2fb92254b3cdf3e1215b488c2bc484d10f87
crown0205 <crown0205@gmail.com>
2022-05-13 14:42:26 +0900

    userInfo 컴포넌트로 나누어 정리 및 정보 불러오기

--- src/components/DetailUser.js
+++ src/components/DetailUser.js
@@ -8,38 +8,48 @@ import { AiOutlineHeart } from 'react-icons/ai';
 const DetailUser = (props) => {
   const urlCheck = props.props.match.url;
   const { detailInfo } = props;
+  let tags = detailInfo.tag;
+  let [tag1, tag2, tag3] = tags.split(',');
 
   return (
     <>
       <UserInfoBox>
-        <div className="userImgWrap">
-          <img
-            className="userImg"
-            src="https://i.pinimg.com/564x/06/25/dd/0625dd9dd3038ffad710f47b283ba4a8.jpg"
-            alt=""
-          />
-        </div>
+        <UserImgWrap>
+          <img className="userImg" src={detailInfo.userProfile} alt="" />
+        </UserImgWrap>
         <div className="userInfo">
-          <div className="userTitle">
-            <p className="tutorName">수자씨</p>
-            <span className="length">Korean</span>/
-            <span className="length">English</span>
-          </div>
-          <p className="contents">
-            한글로 말하고 싶은 외국인이라구요? <br /> 카운잉 스털rrr ~ 밤마늘릐
-            퍼우얼rrr~~~
-          </p>
-          <div className="tags">
-            <span>영어전공</span>
-            <span>약속철저</span>
-            <span>아름다운_목소리</span>
-          </div>
-          <div className="like">
+          <UserTitle>
+            <p className="tutorName">{detailInfo.userName}</p>
+            <span>{detailInfo.language1}</span>/
+            <span>{detailInfo.language2}</span>/
+            <span>{detailInfo.language3}</span>
+          </UserTitle>
+          <Contests>{detailInfo.comment}</Contests>
+          <Tags>
+            {tag1 && <span>{tag1}</span>}
+            {tag2 && <span>{tag2}</span>}
+            {tag3 && <span>{tag3}</span>}
+          </Tags>
+          <Like>
             <AiOutlineHeart className="likeIcon" />
-            539
-          </div>
+            {detailInfo.like}
+          </Like>
         </div>
       </UserInfoBox>
+      {urlCheck ? (
+        ''
+      ) : (
+        <div>
+          <button>좋아요</button>
+          <button
+            onClick={() => {
+              history.push('/mypage');
+            }}
+          >
+            수정
+          </button>
+        </div>
+      )}
     </>
   );
 };
@@ -76,75 +86,75 @@ const UserInfoBox = styled.div`
   display: flex;
   justify-content: space-around;
 
-  .userImgWrap {
-    width: 240px;
-    height: 240px;
-    border-radius: 50%;
-    /* background-color: #eee; */
-    overflow: hidden;
-    .userImg {
-      /* width: 100%; */
-      width: 240px;
-      height: 240px;
-      background-color: red;
-    }
-  }
-
   .userInfo {
     width: 76%;
     height: 240px;
     position: relative;
 
     /* background-color: #686868; */
+  }
+`;
 
-    .userTitle {
-      margin-bottom: 20px;
+const UserImgWrap = styled.div`
+  width: 240px;
+  height: 240px;
+  border-radius: 50%;
+  overflow: hidden;
 
-      .tutorName {
-        display: inline-block;
-        font-size: 34px;
-        font-weight: 600;
-        margin-right: 90px;
-      }
+  .userImg {
+    /* width: 100%; */
+    width: 240px;
+    height: 240px;
+  }
+`;
 
-      .length {
-        display: inline-block;
-        margin: 0 5px;
-        font-size: 26px;
-        color: #5e5e5e;
-      }
-    }
-    .contents {
-      width: 70%;
-      font-size: 30px;
-      margin-bottom: 23px;
-      line-height: 40px;
-    }
+const UserTitle = styled.div`
+  margin-bottom: 20px;
 
-    .tags {
-      span {
-        display: inline-block;
-        font-size: 26px;
-        padding: 12px 22px;
-        margin-left: 20px;
-        border: 2px solid #959595;
-        border-radius: 40px;
-        box-shadow: 0px 2px 6px 0px #00000040;
-        background-color: #fff;
-      }
-    }
+  .tutorName {
+    display: inline-block;
+    font-size: 34px;
+    font-weight: 600;
+    margin-right: 90px;
+  }
 
-    .like {
-      position: absolute;
-      right: 0;
-      top: 0;
-      font-size: 30px;
+  span {
+    display: inline-block;
+    margin: 0 5px;
+    font-size: 26px;
+    color: #5e5e5e;
+  }
+`;
 
-      .likeIcon {
-        font-size: 32px;
-        padding-top: 5px;
-        margin-right: 16px;
-      }
-    }
+const Contests = styled.p`
+  width: 70%;
+  font-size: 30px;
+  margin-bottom: 23px;
+  line-height: 40px;
+`;
+
+const Tags = styled.div`
+  span {
+    display: inline-block;
+    font-size: 26px;
+    padding: 12px 22px;
+    margin-right: 20px;
+    border: 2px solid #959595;
+    border-radius: 40px;
+    box-shadow: 0px 2px 6px 0px #00000040;
+    background-color: #fff;
+  }
+`;
+
+const Like = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  font-size: 30px;
+
+  .likeIcon {
+    font-size: 32px;
+    padding-top: 5px;
+    margin-right: 16px;
   }
 `;
