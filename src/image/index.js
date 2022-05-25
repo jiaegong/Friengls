ef0924fb299a1f17253e30ac3b4d98341a02a489
crown0205 <crown0205@gmail.com>
2022-05-26 04:56:21 +0900

    footer 완성

--- src/image/index.js
+++ src/image/index.js
@@ -2,5 +2,6 @@ import ProfileMedium from './profileMedium.png';
 import MainLogo from './MainLogo.png';
 import BannerImg from './BannerImg.png';
 import Logo from './Logo.svg';
+import footerLogo from './footerLogo.png';
 
-export { ProfileMedium, MainLogo, BannerImg, Logo };
+export { ProfileMedium, MainLogo, BannerImg, Logo, footerLogo };
