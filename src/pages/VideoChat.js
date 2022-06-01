82eff503ab1713ce844fe6c1dcb8279729d598e3
jiaegong <jiae.gong93@gmail.com>
2022-06-01 20:46:30 +0900

    Add: 화면 공유 추가

--- src/pages/VideoChat.js
+++ src/pages/VideoChat.js
@@ -11,6 +11,7 @@ import {
 } from 'react-icons/bs';
 import { GoPlus, GoX } from 'react-icons/go';
 import { MdOutlineRateReview } from 'react-icons/md';
+import { CgScreen } from 'react-icons/cg';
 import { useSelector } from 'react-redux';
 import Translator from '../components/Translator';
 import Chat from '../components/Chat';
@@ -38,13 +39,14 @@ const VideoChat = (props) => {
   const myVideo = useRef();
   const userVideo = useRef();
   const connectionRef = useRef();
-  const senders = useRef();
+  const userStream = useRef();
   const peers = {};
   const roomId = props.match.params.roomName;
   const userName = useSelector((state) => state.user.info); // props로 넘겨주는 게 더 좋을 거 같음
   const userId = userName.userName;
   const [videoOn, setVideoOn] = useState(true);
   const [audioOn, setAudioOn] = useState(true);
+  const [screenOn, setScreenOn] = useState(false);
 
   const socket = io('https://jg-jg.shop', { transports: ['websocket'] });
 
@@ -57,6 +59,7 @@ const VideoChat = (props) => {
         .getUserMedia({ video: true, audio: true }) // 배포 전 true로
         .then((stream) => {
           myVideo.current.srcObject = stream;
+          userStream.current = stream;
 
           // 유저 들어 옴
           if (peer?.id == null) {
@@ -83,7 +86,7 @@ const VideoChat = (props) => {
                 userVideo.current.remove(); // 상대방 나가면 비디오 remove
               });
               peers[userId] = call;
-              connectionRef.current = peer;
+              connectionRef.current = call.peerConnection;
             }
           });
 
@@ -91,13 +94,13 @@ const VideoChat = (props) => {
           peer.on('call', (call) => {
             console.log(5);
             if (call) {
-              call.answer(stream); // 내 stream 보내주기
+              call.answer(userStream.current); // 내 stream 보내주기
               call.on('stream', (userVideoStream) => {
                 console.log(6);
                 userVideo.current.srcObject = userVideoStream; // 상대방 stream 받아오기
               });
             }
-            connectionRef.current = peer;
+            connectionRef.current = call.peerConnection;
           });
         })
         .catch((err) => console.log(err));
@@ -147,21 +150,33 @@ const VideoChat = (props) => {
   };
 
   // 화면 공유
-  function shareScreen() {
-    navigator.mediaDevices.getDisplayMedia({ video: true }).then((stream) => {
-      const screenTrack = stream.getVideoTracks()[0];
-      connectionRef.current.peerConnection
-        .getSenders()
-        .find((sender) => sender.track.kind === screenTrack.kind)
-        .replaceTrack(screenTrack);
-      screenTrack.onended = function () {
-        connectionRef.current.peerConnection
+  const shareScreen = () => {
+    navigator.mediaDevices
+      .getDisplayMedia({
+        video: { cursor: 'always' },
+        audio: { echoCancellation: true, noiseSuppression: true },
+      })
+      .then((stream) => {
+        setScreenOn(!screenOn);
+        myVideo.current.srcObject = stream;
+        const videoTrack = stream.getVideoTracks()[0];
+        console.log(stream);
+        connectionRef.current
           .getSenders()
-          .find((sender) => sender.track.kind === screenTrack.kind)
-          .replaceTrack(myVideo.current.getTracks()[1]);
-      };
-    });
-  }
+          .find((sender) => sender.track.kind === videoTrack.kind)
+          .replaceTrack(videoTrack);
+        videoTrack.onended = function () {
+          setScreenOn(!screenOn);
+          const screenTrack = userStream.current.getVideoTracks()[0];
+          connectionRef.current
+            .getSenders()
+            .find((sender) => sender.track.kind === screenTrack.kind)
+            .replaceTrack(screenTrack);
+          stream.getTracks().forEach((track) => track.stop());
+          myVideo.current.srcObject = userStream.current;
+        };
+      });
+  };
 
   return (
     <Container>
@@ -218,7 +233,7 @@ const VideoChat = (props) => {
           ) : (
             <BsCameraVideoOffFill size={25} onClick={videoHandler} />
           )}
-          <button onClick={shareScreen}>버튼</button>
+          <CgScreen size={25} onClick={shareScreen} />
         </Controllers>
       </RightWrap>
     </Container>
